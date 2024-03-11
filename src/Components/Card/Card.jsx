2bf6708445b1a48f374148e044af28ff139ad694
rjspyk5 <rjspyk5@gmail.com>
2024-03-12 01:29:27 +0600

    visited country counted functionality added

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
-export const CardComponent = ({flag,name,capital}) => {
+export const CardComponent = ({flag,name,capital,handleVisitedCountry}) => {
     const [isVisited, setisVisited] = useState(false)
-const handleIsVisited=()=>setisVisited(!isVisited)
-    console.log(capital);
+const handleIsVisited=(name)=>{
+    setisVisited(!isVisited);
+    handleVisitedCountry(name)
+}
   return (
    <div className='border-8 rounded-xl border-black bg-[#282727] p-2'>
     <div className='grid grid-cols-9'>
@@ -17,8 +19,7 @@ const handleIsVisited=()=>setisVisited(!isVisited)
     <h1 className='text-center font-black text-2xl'>{name}</h1>
     <p className='text-center text-xl my-2'>Capital City</p>
     <h1 className='text-center'>{capital!==undefined ? capital[0]:"Unknown"}</h1>
-    <button onClick={handleIsVisited} className={` ${isVisited? "bg-green-500":"bg-red-500"} rounded-xl text-white px-2 py-1 `}>{isVisited? "Visited":"Visited?"}</button>
-   
+    <button onClick={()=>handleIsVisited(name)} className={` ${isVisited? "bg-green-500":"bg-red-500"} rounded-xl text-white px-2 py-1 `}>{isVisited? "Visited":"Visited?"}</button>
     </div>
    </div>
   )
