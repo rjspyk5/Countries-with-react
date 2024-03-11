16d2a1795f17d17657f289939b93f1ee128705f6
rjspyk5 <rjspyk5@gmail.com>
2024-03-12 01:00:02 +0600

    capital and country naam added on country card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
-export const CardComponent = ({flag,name}) => {
-    console.log(flag,name);
+export const CardComponent = ({flag,name,capital}) => {
+    console.log(capital);
   return (
-   <div className='border-4 bg-slate-600'>
+   <div className='border-8 rounded-xl border-black bg-[#282727] p-2'>
     <div className='grid grid-cols-9'>
-        <div className="col-span-4"></div>
-        <div className="col-span-5 bg-white"></div>
+        <div className="col-span-4 m-2 ">
+            <img className='w-full h-12 rounded-md' src={flag} alt="" />
+        </div>
+        <div className="col-span-5 bg-white rounded-t-xl ">
+        </div>
     </div>
-    <div className='bg-white'>
+    <div className='bg-white rounded-b-xl rounded-tl-xl p-5 ' > 
+    <h1 className='text-center font-black text-2xl'>{name}</h1>
+    <p className='text-center text-xl my-2'>Capital City</p>
+    <h1 className='text-center'>{capital!==undefined ? capital[0]:"Unknown"}</h1>
     </div>
    </div>
   )
--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -7,11 +7,12 @@ import dataContext from "../Context/Context";
 export const Countries = () => {
   const first = useContext(dataContext);
   return (
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+  <div className="grid grid-cols-1 mx-5 md:grid-cols-2 lg:grid-cols-4 gap-10">
     {first && first.map((el,iDx)=>{
+      console.log(el);
       const {name:{common:naam},population,flags:{png:flag}}=el;
     
-    return <CardComponent key={iDx} name={naam} flag={flag} />})}
+    return <CardComponent key={iDx} name={naam} flag={flag} capital={el?.capital} />})}
  
   </div>);
 };
